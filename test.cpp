44d8fd13de5240c14b88e975286849ba4158cb3c
CommodoreKrusty <miracle.glue@gmail.com>
2025-03-09 07:49:03 -0300

    template inheritance

--- test.cpp
+++ test.cpp
@@ -1,14 +1,52 @@
 #include <iostream>
-#include "MyStatistics.h"
+#include <random>
+#include <time.h>
+#include <list>
+#include <algorithm>
+#include <cstdlib>
+#include "/home/paul/Documents/c++/statistics/MyStatistics.h"
 
-using std::cerr;
+using std::mt19937, std::div, std::div_t, std::list, std::for_each, ::cerr;
+
+template < class Key, int T> class SuperMyStatistics : public MyStatistics< Key, T>{
+    public:
+    SuperMyStatistics(){}
+    ~SuperMyStatistics(){
+//        this->data.empty();
+    }
+};
+
+typedef SuperMyStatistics<double, 2> dataset;
+typedef list<dataset> datasets;
+
+void FillDataSet(mt19937 rnd, dataset* s);
 
 int main(){
-    MyStatistics<int, 5> data;
- //   MyStatistics<int, 5>::newArray na = {2, 5, 4, 23, 12};
-    data.addRecord({2, 5, 4, 23, 12});
-    data.addRecord({16, 44, 20, 51, 22});
-    data.print();
-    cerr << "record count: " << data.recordCount() << "\n";
+    mt19937 rnd(time(NULL));
+    datasets mydata;
+    dataset s;
+    FillDataSet(rnd, &s);
+    mydata.push_back(s);
+
+    auto print = [](dataset v){
+        v.print();
+    };
+
+    for_each(mydata.begin(), mydata.end(), print);
     return 0;
+}
+
+void FillDataSet(mt19937 rnd, dataset* s){
+    for(int i = 0; i < 10; i++){
+        dataset::MyArray j;
+
+        div_t a = div(rnd() % 100 + 1, 10);
+        div_t b = div(rnd() % 100 + 1, 10);
+        
+        j[0] = (double) a.quot + (double)a.rem / 10;
+        j[1] = (double) b.quot + (double)b.rem / 10;
+
+        s->addRecord(j);
+    }
+
 }
